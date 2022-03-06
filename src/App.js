156ac205ac65eaaf2909c17fb114c3b824df04e7
usfm-templates <usfm.templates@protonmail.com>
2022-03-05 23:26:34 -0800

    master

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/us-familymarketing-logo.png";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+    <header className="flex justify-center items-center h-screen ">
+      <div className="rounded-xl pb-7 px-10 border z-100 bg-white floatbub shadow-md">
+        <img src={logo} className="h-24 m-auto" alt="logo" />
+        <div className="flex flex-col">
+          <a
+            href="https://familymarketing.us"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className=" min-w-full	 font-bold font-mono text-center px-10 py-3 m-2 rounded-full border border-gray-400 cosmo">
+              US Family Marketing
+            </button>
+          </a>
+
+          <a
+            href="https://www.facebook.com/unitedstatesfamilymarketing"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className="min-w-full	 font-bold font-mono text-center px-10 py-3 m-2 rounded-full border border-gray-400 cosmo">
+              Facebook
+            </button>
+          </a>
+
+          <a
+            href="https://familymarketing.us"
+            target="_blank"
+            rel="noopener noreferrer" 
+          >
+            <button className="min-w-full	 font-bold font-mono text-center px-10 py-3 m-2 rounded-full border border-gray-400 cosmo">
+              Coming soon...
+            </button>
+          </a>
+        </div>
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
+        </a> */}
+      </div>
+    </header>
   );
 }
 
